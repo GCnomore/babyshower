e54a8c2c71c9ed29cad410d9720b8bcde10d59cf
isaac <isaacgc08@gmail.com>
2022-02-13 18:36:13 -0800

    proto5

--- main.js
+++ main.js
@@ -2,10 +2,33 @@ const card = document.querySelector(".card");
 const cardFront = document.querySelector(".cardFront");
 const cardback = document.querySelector(".cardBack");
 
+const txt1 = document.querySelector(".txt1");
+const txt2 = document.querySelector(".txt2");
+const txt3 = document.querySelector(".txt3");
+
 card.addEventListener("click", (e) => {
   card.classList.add("rotate");
   setTimeout(() => {
     cardFront.classList.add("hidden");
     cardback.classList.remove("hidden");
-  }, 1000);
+  }, 400);
 });
+
+setTimeout(() => {
+  txt1.classList.remove("hide");
+  txt1.classList.add("fadeIn");
+}, 500);
+
+setTimeout(() => {
+  txt2.classList.remove("hide");
+  txt2.classList.add("fadeIn");
+}, 2500);
+
+setTimeout(() => {
+  txt3.classList.remove("hide");
+  txt3.classList.add("fadeIn");
+}, 5000);
+
+setTimeout(() => {
+  window.scroll({ behavior: "smooth", top: 1000 });
+}, 8000);
